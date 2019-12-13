e151165c2f8ecfb710f82b7dc1df5707931aae5e
kaliga <kaliga@uitoux.in>
2019-12-13 17:16:24 +0530

    CaseCancelledReason model

---
+++ src/models/CaseCancelledReason.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Abs\RsaCasePkg;
+
+use Abs\HelperPkg\Traits\SeederTrait;
+use App\Company;
+use App\Config;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CaseCancelledReason extends Model {
+	use SeederTrait;
+	use SoftDeletes;
+	protected $table = 'case_cancelled_reasons';
+	protected $fillable = [
+		'company_id',
+		'name',
+	];
+
+	
+	public function company() {
+		return $this->belongsTo('App\Company', 'company_id');
+	}
+	public function createdBy() {
+		return $this->belongsTo('App\User', 'created_by_id');
+	}
+
+	public function updatedBy() {
+		return $this->belongsTo('App\User', 'updated_by_id');
+	}
+
+	public function deletedBy() {
+		return $this->belongsTo('App\User', 'deleted_by_id');
+	}
+
+	public static function createFromObject($record_data) {
+
+		$errors = [];
+		$company = Company::where('code', $record_data->company)->first();
+		if (!$company) {
+			dump('Invalid Company : ' . $record_data->company);
+			return;
+		}
+
+		$admin = $company->admin();
+		if (!$admin) {
+			dump('Default Admin user not found');
+			return;
+		}
+
+		$type = Config::where('name', $record_data->type)->where('config_type_id', 89)->first();
+		if (!$type) {
+			$errors[] = 'Invalid Tax Type : ' . $record_data->type;
+		}
+
+		if (count($errors) > 0) {
+			dump($errors);
+			return;
+		}
+
+		$record = self::firstOrNew([
+			'company_id' => $company->id,
+			'name' => $record_data->tax_name,
+		]);
+		$record->type_id = $type->id;
+		$record->created_by_id = $admin->id;
+		$record->save();
+		return $record;
+	}
+
+}

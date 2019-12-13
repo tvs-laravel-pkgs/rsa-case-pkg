464a33ae18bdea5c8cffd80e8035fb92b9826f09
Abdul <abdul@uitoux.in>
2019-12-13 20:20:42 +0530

    u

--- src/database/seeds/RsaCasePkgPermissionSeeder.php
+++ src/database/seeds/RsaCasePkgPermissionSeeder.php
@@ -12,40 +12,110 @@ class RsaCasePkgPermissionSeeder extends Seeder {
 	 */
 	public function run() {
 		$permissions = [
-			//MASTER > caseS
-			4600 => [
-				'display_order' => 10,
-				'parent_id' => null,
-				'name' => 'cases',
-				'display_name' => 'RsaCases',
+			//ACTIVITIES
+			[
+				'display_order' => 99,
+				'parent' => null,
+				'name' => 'activities',
+				'display_name' => 'Activities Menu',
 			],
-			4601 => [
+
+			[
+				'display_order' => 99,
+				'parent' => null,
+				'name' => 'activity-status',
+				'display_name' => 'Activities Status',
+			],
+			[
 				'display_order' => 1,
-				'parent_id' => 4600,
-				'name' => 'add-case',
-				'display_name' => 'Add',
-			],
-			4602 => [
-				'display_order' => 2,
-				'parent_id' => 4600,
-				'name' => 'edit-case',
+				'parent' => 'activity-status',
+				'name' => 'edit-activities',
 				'display_name' => 'Edit',
 			],
-			4603 => [
-				'display_order' => 3,
-				'parent_id' => 4600,
-				'name' => 'delete-case',
+			[
+				'display_order' => 2,
+				'parent' => 'activity-status',
+				'name' => 'delete-activities',
 				'display_name' => 'Delete',
+			],
+			[
+				'display_order' => 3,
+				'parent' => 'activity-status',
+				'name' => 'delete-activities',
+				'display_name' => 'Delete',
+			],
+			[
+				'display_order' => 4,
+				'parent' => 'activity-status',
+				'name' => 'export-activities',
+				'display_name' => 'Export',
+			],
+			[
+				'display_order' => 5,
+				'parent' => 'activity-status',
+				'name' => 'view-all-activities',
+				'display_name' => 'View All',
+			],
+			[
+				'display_order' => 5,
+				'parent' => 'activity-status',
+				'name' => 'view-mapped-state-activities',
+				'display_name' => 'Only Mapped States',
+			],
+			[
+				'display_order' => 5,
+				'parent' => 'activity-status',
+				'name' => 'view-own-activities',
+				'display_name' => 'View Only Own',
+			],
+
+			//ACTIVITIES VERIFICATION
+			[
+				'display_order' => 99,
+				'parent' => null,
+				'name' => 'activity-verification',
+				'display_name' => 'Activity Verification',
+			],
+			[
+				'display_order' => 6,
+				'parent' => 'activity-verification',
+				'name' => 'verify-all-activities',
+				'display_name' => 'Verify All',
+			],
+			[
+				'display_order' => 6,
+				'parent' => 'activity-verification',
+				'name' => 'verify-mapped-activities',
+				'display_name' => 'Only Mapped',
+			],
+
+			//ASP INVOICES
+			[
+				'display_order' => 99,
+				'parent' => null,
+				'name' => 'asp-invoices',
+				'display_name' => 'ASP Invoices',
+			],
+			[
+				'display_order' => 6,
+				'parent' => 'asp-invoices',
+				'name' => 'view-all-asp-invoices',
+				'display_name' => 'View All',
+			],
+			[
+				'display_order' => 6,
+				'parent' => 'asp-invoices',
+				'name' => 'view-only-state-asp-invoices',
+				'display_name' => 'Only State Mapped',
+			],
+			[
+				'display_order' => 6,
+				'parent' => 'asp-invoices',
+				'name' => 'view-only-own-asp-invoices',
+				'display_name' => 'Only Own',
 			],
 
 		];
-
-		foreach ($permissions as $permission_id => $permsion) {
-			$permission = Permission::firstOrNew([
-				'id' => $permission_id,
-			]);
-			$permission->fill($permsion);
-			$permission->save();
-		}
+		Permission::createFromArrays($permissions);
 	}
 }
--- src/public/angular/rsa-case-pkg/routes.js
+++ src/public/angular/rsa-case-pkg/routes.js
@@ -13,5 +13,37 @@ app.config(['$routeProvider', function($routeProvider) {
     when('/case-pkg/case/edit/:id', {
         template: '<case-form></case-form>',
         title: 'Edit RsaCase',
-    });
+    }).
+
+    //ACTIVITIES
+    when('/rsa-case-pkg/activity-status/list', {
+        template: '<activity-status-list></activity-status-list>',
+        title: 'Activity Status',
+    }).
+    when('/rsa-case-pkg/activity-status/view/:id', {
+        template: '<activity-status-view></activity-status-view>',
+        title: 'Activity Status View',
+    }).
+
+    //ACTIVITY VERIFICATION
+    when('/rsa-case-pkg/activity-verification/list', {
+        template: '<activity-verification-list></activity-verification-list>',
+        title: 'Activity Verification',
+    }).
+    when('/rsa-case-pkg/activity-verification/view/:id', {
+        template: '<activity-verification-view></activity-verification-view>',
+        title: 'Activity Verification View',
+    }).
+
+    //ASP INVOICES
+    when('/rsa-case-pkg/asp - invoice / list', {
+        template: '<asp-invoice-list></asp-invoice-list>',
+        title: 'ASP Invoices',
+    }).
+    when('/rsa-case-pkg/asp-invoice/view/:id', {
+        template: '<asp-invoice-view></asp-invoice-view>',
+        title: 'ASP Invoice View',
+    })
+
+    ;
 }]);

81c0b7b6827452c5f8f99342aaf292ddf9f7ad6a
Ramakrishnan <45380286+ramuitoux@users.noreply.github.com>
2019-12-13 11:20:31 +0530

    migrations

---
+++ src/database/migrations/2019_12_13_111323_cases_po_c.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CasesPoC extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('cases', function (Blueprint $table) {
+			$table->increments('id');
+			$table->unsignedInteger('company_id');
+			$table->foreign('company_id')->references('id')->on('companies')->onDelete('CASCADE')->onUpdate('cascade');
+			$table->string('number', 32);
+			$table->unique(["company_id", "number"]);
+			$table->datetime('date');
+			$table->unsignedInteger('call_center_id');
+			$table->foreign('call_center_id')->references('id')->on('call_centers')->onDelete('CASCADE')->onUpdate('cascade');
+			$table->unsignedInteger('client_id');
+			$table->foreign('client_id')->references('id')->on('clients')->onDelete('CASCADE')->onUpdate('cascade');
+			$table->string('customer_name', 255);
+			$table->string('customer_contact_number', 15);
+			$table->string('contact_name', 50)->nullable();
+			$table->string('contact_number', 15)->nullable();
+			$table->unsignedInteger('cancel_reason_id')->nullable();
+			$table->foreign('cancel_reason_id')->references('id')->on('case_cancel_reasons')->onDelete('SET NULL')->onUpdate('cascade');
+			$table->datetime('data_filled_date');
+			$table->string('vehicle_registration_number', 24);
+			$table->string('vin_no', 24)->nullable();
+			$table->unsignedInteger('eligibility_type_id')->nullable();
+			$table->foreign('eligibility_type_id')->references('id')->on('entities')->onDelete('SET NULL')->onUpdate('cascade');
+			$table->unsignedInteger('vehicle_model_id')->nullable();
+			$table->foreign('vehicle_model_id')->references('id')->on('vehicle_models')->onDelete('SET NULL')->onUpdate('cascade');
+			$table->unsignedInteger('subject_id')->nullable();
+			$table->foreign('subject_id')->references('id')->on('subjects')->onDelete('SET NULL')->onUpdate('cascade');
+			$table->unsignedInteger('policy_id')->nullable();
+			$table->foreign('policy_id')->references('id')->on('policies')->onDelete('SET NULL')->onUpdate('cascade');
+			$table->unsignedInteger('status_id')->nullable();
+			$table->foreign('status_id')->references('id')->on('case_statuses')->onDelete('SET NULL')->onUpdate('cascade');
+			$table->unsignedDecimal('km_during_breakdown', 11, 2)->nullable();
+			$table->unsignedDecimal('bd_lat', 11, 2)->nullable();
+			$table->unsignedDecimal('bd_long', 11, 2)->nullable();
+			$table->string('bd_location', 191)->nullable();
+			$table->unsignedInteger('bd_city_id')->nullable();
+			$table->foreign('bd_city_id')->references('id')->on('districts')->onDelete('CASCADE')->onUpdate('cascade');
+			$table->unsignedInteger('created_by_id')->nullable();
+			$table->foreign('created_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
+			$table->unsignedInteger('updated_by_id')->nullable();
+			$table->foreign('updated_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
+			$table->unsignedInteger('deleted_by_id')->nullable();
+			$table->foreign('deleted_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('cases');
+	}
+}

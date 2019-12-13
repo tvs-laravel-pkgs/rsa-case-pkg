3bea7cde7a8ac9d2f4541625d57837083d0b32cf
Ramakrishnan <45380286+ramuitoux@users.noreply.github.com>
2019-12-13 17:28:06 +0530

    case api updates

---
+++ src/database/migrations/2019_12_13_172115_cases_u.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CasesU extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('cases', function (Blueprint $table) {
+			$table->string('membership_number', 50)->nullable()->after('membership_type_id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('cases', function (Blueprint $table) {
+			$table->dropColumn('membership_number');
+		});
+	}
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -2,7 +2,12 @@
 Route::group(['namespace' => 'Abs\RsaCasePkg\Api', 'middleware' => ['api']], function () {
 	Route::group(['prefix' => 'case-pkg/api'], function () {
 		Route::group(['middleware' => ['auth:api']], function () {
-			// Route::get('taxes/get', 'TaxController@getTaxes');
+			Route::post('case/save', 'Api\CaseController@saveCase');
+			Route::post('activity/save', 'Api\CaseController@saveActivity');
+			Route::post('create-invoice', 'Api\InvoiceController@createInvoice');
+			Route::post('get-eligible-po-list', 'Api\CaseController@getEligiblePOList');
+			Route::post('get-invoice-list', 'Api\InvoiceController@getInvoiceList');
+			Route::post('get-invoice-details', 'Api\InvoiceController@getInvoiceDetails');
 		});
 	});
 });

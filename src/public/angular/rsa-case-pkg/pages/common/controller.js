2f22a8ac8e63d832386b35ca7f49fd9e5515aa31
Abdul <abdul@uitoux.in>
2020-01-03 17:38:27 +0530

    activity view updates

--- src/public/angular/rsa-case-pkg/pages/common/controller.js
+++ src/public/angular/rsa-case-pkg/pages/common/controller.js
@@ -11,6 +11,19 @@ app.component('aspActivitiesHeader', {
 
 //----------------------------------------------------------------------------------------------------------------------------
 
+app.component('caseDetailHeader', {
+    templateUrl: activity_status_view_tab_header_template_url,
+    bindings: {
+        data: '<',
+    },
+    controller: function($http, HelperService, $scope, $rootScope, $routeParams, $location) {
+        $scope.loading = true;
+        var self = this;
+    }
+});
+
+//----------------------------------------------------------------------------------------------------------------------------
+
 app.component('aspDetails', {
     templateUrl: activity_status_view_asp_details_template_url,
     bindings: {

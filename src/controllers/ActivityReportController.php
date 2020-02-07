9e765ce6b3142358a911d0ded299bfcbd57ff918
Vijay S <vijay@uitoux.in>
2020-02-07 17:59:30 +0530

    reconciliation report completed

--- src/controllers/ActivityReportController.php
+++ src/controllers/ActivityReportController.php
@@ -40,15 +40,15 @@ class ActivityReportController extends Controller {
 			->join('service_types', 'service_types.id', 'activities.service_type_id')
 			->leftJoin('activity_details as bo_km_travelled', function ($join) {
 				$join->on('bo_km_travelled.activity_id', 'activities.id')
-					->where('bo_km_travelled.key_id', 158);
+					->where('bo_km_travelled.key_id', 158); //BO KM Travelled
 			})
 			->leftJoin('activity_details as bo_paid_amt', function ($join) {
 				$join->on('bo_paid_amt.activity_id', 'activities.id')
-					->where('bo_paid_amt.key_id', 182);
+					->where('bo_paid_amt.key_id', 182); //BO AMOUNT
 			})
 			->join('Invoices as invoice', function ($join) {
 				$join->on('Invoice.id', 'activities.invoice_id')
-					->where('invoice.status_id', 2);
+					->where('invoice.status_id', 2); //PAID
 			})
 			->where('activities.is_exceptional_check', 0)
 			->where('cases.company_id', Auth::user()->company_id)
@@ -86,39 +86,46 @@ class ActivityReportController extends Controller {
 	public function getReconciliationReport() {
 		$user_id = Auth::user()->id;
 
+		$total_amount_paid_in_year = Activity::join('activity_details', function ($join) {
+			$join->on('activity_details.activity_id', 'activities.id')
+				->where('activity_details.key_id', 182); //BO AMOUNT
+		})
+			->where('activities.status_id', 14) //PAID
+			->whereYear('activities.updated_at', date('Y'))
+			->sum('activity_details.value')
+		;
+
 		$total_amount_submit_in_year = Activity::select(
 			'activities.id',
-			DB::raw('IF(sum(activity_details.value) IS NULL or sum(activity_details.value) = "", 0, sum(activity_details.value)) as total'),
-			DB::raw('DATE_FORMAT(logs.updated_at,"%b") as month')
+			DB::raw('sum(activity_details.value) as total'),
+			DB::raw('DATE_FORMAT(activities.updated_at,"%b") as month')
 		)
 			->join('activity_details', function ($join) {
 				$join->on('activity_details.activity_id', 'activities.id')
-					->where('activity_details.key_id', 182);
+					->where('activity_details.key_id', 182); //BO AMOUNT
 			})
-			->join('Invoices', 'Invoices.id', 'activities.invoice_id')
-			->join('logs', 'activities.id', 'logs.entity_id')
-			->whereYear('logs.updated_at', date('Y'))
-			->where('logs.type_id', 2)
-			->where('Invoices.status_id', '!=', 2) //EXCEPT PAID
-			->orderBy('logs.updated_at', 'ASC')
+			->whereYear('activities.updated_at', date('Y'))
+			->where('activities.status_id', 12) //INVOICED-WAITING FOR PAYMENT
+			->orWhere('activities.status_id', 13) //PAYMENT INPROGRESS
 			->groupby('month')
+			->orderBy('activities.updated_at', 'ASC')
 			->pluck('total', 'month')
 			->toArray()
 		;
 
 		$amount_bills_yet_to_receive = Activity::select(
 			'activities.id',
-			DB::raw('IF(sum(activity_details.value) IS NULL or sum(activity_details.value) = "", 0, sum(activity_details.value)) as total'),
+			DB::raw('sum(activity_details.value) as total'),
 			DB::raw('DATE_FORMAT(activities.updated_at,"%b") as month')
 		)
 			->join('activity_details', function ($join) {
 				$join->on('activities.id', 'activity_details.activity_id')
-					->where('activity_details.key_id', 182);
+					->where('activity_details.key_id', 182); //BO AMOUNT
 			})
 			->where(function ($query) {
 				$query->whereYear('activities.updated_at', date('Y'))
-					->where('activities.status_id', 1)
-					->orWhere('activities.status_id', 11);
+					->where('activities.status_id', 1) //CASE CLOSED WAITING FOR ASP GENERATE INVOICE
+					->orWhere('activities.status_id', 11); //BP APPROVED WAITING FOR ASP INVOICE GENERATION
 			})
 			->groupBy('month')
 			->pluck('total', 'month')
@@ -126,14 +133,12 @@ class ActivityReportController extends Controller {
 		;
 
 		$count_bills_yet_to_receive = Activity::select(
-			DB::raw('IF(count(activities.id) IS NULL or count(activities.id) = "", 0, count(activities.id)) as total'),
-			DB::raw('DATE_FORMAT(activities.updated_at,"%b") month')
+			DB::raw('count(id) as total'),
+			DB::raw('DATE_FORMAT(updated_at,"%b") month')
 		)
-			->where(function ($query) {
-				$query->whereYear('activities.updated_at', date('Y'))
-					->where('activities.status_id', 1)
-					->orWhere('activities.status_id', 11);
-			})
+			->whereYear('updated_at', date('Y'))
+			->where('activities.status_id', 1) //CASE CLOSED WAITING FOR ASP GENERATE INVOICE
+			->orWhere('activities.status_id', 11) //BP APPROVED WAITING FOR ASP INVOICE GENERATION
 			->groupBy('month')
 			->pluck('total', 'month')
 			->toArray()
@@ -141,76 +146,114 @@ class ActivityReportController extends Controller {
 
 		$total_count_submit_in_year = Activity::select(
 			'activities.id',
-			DB::raw('IF(count(activity_details.value) IS NULL or count(activity_details.value) = "", 0, count(activity_details.value)) as total'),
-			DB::raw('DATE_FORMAT(logs.updated_at,"%b") as month')
+			DB::raw('count(activity_details.value) as total'),
+			DB::raw('DATE_FORMAT(activities.updated_at,"%b") as month')
 		)
 			->join('activity_details', function ($join) {
 				$join->on('activity_details.activity_id', 'activities.id')
-					->where('activity_details.key_id', 182);
+					->where('activity_details.key_id', 182); //BO AMOUNT
 			})
-			->join('Invoices', 'Invoices.id', 'activities.invoice_id')
-			->join('logs', 'activities.id', 'logs.entity_id')
-			->whereYear('logs.updated_at', date('Y'))
-			->where('logs.type_id', 2)
-			->where('Invoices.status_id', '!=', 2) //EXCEPT PAID
-			->orderBy('logs.updated_at', 'ASC')
+			->whereYear('activities.updated_at', date('Y'))
+			->where('activities.status_id', 12) //INVOICED-WAITING FOR PAYMENT
+			->orWhere('activities.status_id', 13) //PAYMENT INPROGRESS
+			->orderBy('activities.updated_at', 'ASC')
 			->groupby('month')
 			->pluck('total', 'month')
 			->toArray()
 		;
 
-		$total_amount_paid_in_year = Activity::join('activity_details', function ($join) {
-			$join->on('activity_details.activity_id', 'activities.id')
-				->where('activity_details.key_id', 182);
-		})
-			->where('activities.status_id', 14)
-			->whereYear('activities.updated_at', date('Y'))
-			->sum('activity_details.value')
-		;
-
 		$amount_of_tickets_submitted = Activity::join('activity_details', function ($join) {
 			$join->on('activity_details.activity_id', 'activities.id')
-				->where('activity_details.key_id', 182);
+				->where('activity_details.key_id', 182); //BO AMOUNT
 		})
-			->join('logs', 'activities.id', 'logs.entity_id')
-		//->where('logs.action', config('constants.ticket_statuses.ASP_CONFIRMED_INVOICE_TO_APPROVED_BATCH'))
-			->where('activities.status_id', 12)
-			->orWhere('activities.status_id', 13)
-			->where('logs.type_id', 2)
-			->whereYear('logs.updated_at', date('Y'))
-			->groupBy('logs.entity_id')
-			->pluck('activity_details.value')
-			->toArray()
-		// ->get()
+			->where('activities.status_id', 12) //INVOICED-WAITING FOR PAYMENT
+			->orWhere('activities.status_id', 13) //PAYMENT INPROGRESS
+			->whereYear('activities.updated_at', date('Y'))
+			->sum('activity_details.value')
 		;
 
 		$amount_of_bills_yet_to_receive = Activity::join('activity_details', function ($join) {
 			$join->on('activity_details.activity_id', 'activities.id')
-				->where('activity_details.key_id', 182);
+				->where('activity_details.key_id', 182); //BO AMOUNT
 		})
 			->whereYear('activities.updated_at', date('Y'))
-			->where('activities.status_id', 1)
-			->orWhere('activities.status_id', 11)
-		// ->pluck('activity_details.value')
+			->where('activities.status_id', 1) //CASE CLOSED WAITING FOR ASP GENERATE INVOICE
+			->orWhere('activities.status_id', 11) //BP APPROVED WAITING FOR ASP INVOICE GENERATION
 			->sum('activity_details.value')
 		;
+
+		$total_count_of_tickets_in_year = Activity::where('status_id', 14) //PAID
+			->whereYear('updated_at', date('Y'))
+			->count();
+
+		$total_count_of_tickets_submitted = Activity::where('status_id', 12) //INVOICED-WAITING FOR PAYMENT
+			->orWhere('status_id', 13) //PAYMENT INPROGRESS
+			->whereYear('updated_at', date('Y'))
+			->pluck('id')
+			->toArray()
+		;
+
+		$bills_yet_to_receive = Activity::where('activities.status_id', 1) //CASE CLOSED WAITING FOR ASP GENERATE INVOICE
+			->orWhere('activities.status_id', 11) //BP APPROVED WAITING FOR ASP INVOICE GENERATION
+			->whereYear('activities.updated_at', date('Y'))
+			->count()
+		;
+
 		$this->data['extras'] = [
 			'total_amount_submit_in_year_chart' => $total_amount_submit_in_year,
 			'amount_of_bills_yet_to_receive_chart' => $amount_bills_yet_to_receive,
 			'total_count_yet_to_receive_in_year_chart' => $count_bills_yet_to_receive,
 			'total_count_submit_in_year_chart' => $total_count_submit_in_year,
 			'total_amount_paid_in_year' => $total_amount_paid_in_year,
-			'amount_of_tickets_submitted' => array_sum($amount_of_tickets_submitted),
+			'amount_of_tickets_submitted' => $amount_of_tickets_submitted,
 			'amount_of_bills_yet_to_receive' => $amount_of_bills_yet_to_receive,
-
+			'total_count_of_tickets_in_year' => $total_count_of_tickets_in_year,
+			'total_count_of_tickets_submitted' => count($total_count_of_tickets_submitted),
+			'bills_yet_to_receive' => $bills_yet_to_receive,
 		];
-		// $this->data['total_amount_submit_in_year_chart'] = $total_amount_submit_in_year;
-		// $this->data['amount_of_bills_yet_to_receive_chart'] = $amount_bills_yet_to_receive;
-		// $this->data['total_count_yet_to_receive_in_year_chart'] = $count_bills_yet_to_receive;
-		// $this->data['total_count_submit_in_year_chart'] = $total_count_submit_in_year;
-		// $this->data['total_amount_paid_in_year'] = $total_amount_paid_in_year;
-		// $this->data['amount_of_tickets_submitted'] = array_sum($amount_of_tickets_submitted);
-		// $this->data['amount_of_bills_yet_to_receive'] = $amount_of_bills_yet_to_receive;
+
+		$Total_amount_paid = Activity::select(
+			DB::raw('sum(activity_details.value) as total'),
+			DB::raw('DATE_FORMAT(activities.updated_at,"%b") as month')
+		)
+			->join('activity_details', function ($join) {
+				$join->on('activity_details.activity_id', 'activities.id')
+					->where('activity_details.key_id', 182); //BO AMOUNT
+			})
+			->whereYear('activities.updated_at', date('Y'))
+			->where('activities.status_id', 14) //PAID
+			->groupBy('month')
+			->pluck('total', 'month')
+			->toArray()
+		;
+
+		$Total_count_of_tickets = Activity::select(
+			DB::raw('count(id) as total'),
+			DB::raw('DATE_FORMAT(updated_at,"%b") month'))
+			->whereYear('updated_at', date('Y'))
+			->where('status_id', 14) //PAID
+			->groupby('month')
+			->pluck('total', 'month')
+			->toArray();
+
+		for ($i = 1; $i <= 12; $i++) {
+			if (date('m') >= $i) {
+				$months[] = $i;
+				$monthes[] = date('M', strtotime('01-' . $i . '-' . date('Y')));
+			}
+		}
+		$this->data['monthes'] = $monthes;
+
+		//MONTHLY INFORMATION TABLE DATA
+		foreach ($months as $month) {
+			$month = date('M', strtotime('01-' . $month . '-' . date('Y')));
+			$this->data['month_wise_data']['Total_amount_paids'][$month] = array_key_exists($month, $Total_amount_paid) ? $Total_amount_paid[$month] : 0;
+			$this->data['month_wise_data']['Total_count_of_tickets'][$month] = array_key_exists($month, $Total_count_of_tickets) ? $Total_count_of_tickets[$month] : 0;
+			$this->data['month_wise_data']['total_amount_submit_in_year'][$month] = array_key_exists($month, $total_amount_submit_in_year) ? $total_amount_submit_in_year[$month] : 0;
+			$this->data['month_wise_data']['total_count_submit_in_year'][$month] = array_key_exists($month, $total_count_submit_in_year) ? $total_count_submit_in_year[$month] : 0;
+			$this->data['month_wise_data']['amount_bills_yet_to_receive'][$month] = array_key_exists($month, $amount_bills_yet_to_receive) ? $amount_bills_yet_to_receive[$month] : 0;
+			$this->data['month_wise_data']['count_bills_yet_to_receive'][$month] = array_key_exists($month, $count_bills_yet_to_receive) ? $count_bills_yet_to_receive[$month] : 0;
+		}
 
 		return response()->json($this->data);
 	}
--- src/public/angular/rsa-case-pkg/pages/reconciliation-report/controller.js
+++ src/public/angular/rsa-case-pkg/pages/reconciliation-report/controller.js
@@ -8,117 +8,158 @@ app.component('reconciliationReportView', {
         $http.get(
             laravel_routes['getReconciliationReport']
         ).then(function(response) {
-            console.log(response);
+            // console.log(response);
             self.extras = response.data.extras;
+            self.monthes = response.data.monthes;
+            self.data = response.data.month_wise_data;
             var d = new Date();
             var Curr_Year = d.getFullYear();
             self.current_year = Curr_Year;
             var num_month = d.getMonth();
             var months = [];
             var all_months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+            for (var i = 0; i <= num_month; i++) {
+                months[i] = all_months[i];
+            }
+
+            var amounts1 = [];
+            var amounts2 = [];
+            angular.forEach(self.extras.total_amount_submit_in_year_chart, function(value, key) {
+                amounts1[key] = value;
+            });
+            angular.forEach(self.extras.amount_of_bills_yet_to_receive_chart, function(value, key) {
+                amounts2[key] = value;
+            });
+            for (var i in months) {
+                var month = months[i];
+                console.log(month);
+                if (typeof(amounts1[month]) == 'undefined') {
+                    amounts1[i] = 0;
+                } else {
+                    amounts1[i] = amounts1[month];
+                }
+                console.log(amounts1);
+                if (typeof(amounts2[month]) == 'undefined') {
+                    amounts2[i] = 0;
+                } else {
+                    amounts2[i] = amounts2[month];
+                }
+            }
+
             // /* Chart */
-            // Highcharts.chart('chart1', {
+            Highcharts.chart('chart1', {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: ''
+                },
+                xAxis: {
+                    categories: months,
+                },
+                yAxis: {
+                    allowDecimals: false,
+                    min: 0,
+                    title: {
+                        text: 'Amounts'
+                    }
+                },
+                tooltip: {
+                    formatter: function() {
+                        return '<b>' + this.x + '</b><br/>' +
+                            this.series.name + ': ' + this.y + '<br/>' +
+                            'Total: ' + this.point.stackTotal;
+                    }
+                },
+                plotOptions: {
+                    column: {
+                        stacking: 'normal',
+                        pointWidth: 30
+                    }
+                },
+                series: [{
+                    name: '',
+                    data: amounts1,
+                    stack: '',
+                    color: '#fa8c46'
+                }, {
+                    name: '',
+                    data: amounts2,
+                    stack: '',
+                    color: '#216ff2'
 
-            //     chart: {
-            //         type: 'column'
-            //     },
+                }]
+            });
 
-            //     title: {
-            //         text: ''
-            //     },
-
-            //     xAxis: {
-            //         categories: months,
-            //     },
-
-            //     yAxis: {
-            //         allowDecimals: false,
-            //         min: 0,
-            //         title: {
-            //             text: 'Amounts'
-            //         }
-            //     },
-
-            //     tooltip: {
-            //         formatter: function() {
-            //             return '<b>' + this.x + '</b><br/>' +
-            //                 this.series.name + ': ' + this.y + '<br/>' +
-            //                 'Total: ' + this.point.stackTotal;
-            //         }
-            //     },
-
-            //     plotOptions: {
-            //         column: {
-            //             stacking: 'normal',
-            //             pointWidth: 30
-            //         }
-            //     },
-
-            //     series: [{
-            //         name: '',
-            //         data: amounts1,
-            //         stack: '',
-            //         color: '#fa8c46'
-            //     }, {
-            //         name: '',
-            //         data: amounts2,
-            //         stack: '',
-            //         color: '#216ff2'
-
-            //     }]
-            // });
+            var counts1 = [];
+            var counts2 = [];
+            angular.forEach(self.extras.total_count_submit_in_year_chart, function(value, key) {
+                counts1[key] = value;
+            });
+            angular.forEach(self.extras.total_count_yet_to_receive_in_year_chart, function(value, key) {
+                counts2[key] = value;
+            });
+            for (var i in months) {
+                var month = months[i];
+                console.log(month);
+                if (typeof(counts1[month]) == 'undefined') {
+                    counts1[i] = 0;
+                } else {
+                    counts1[i] = counts1[month];
+                }
+                console.log(counts1);
+                if (typeof(counts2[month]) == 'undefined') {
+                    counts2[i] = 0;
+                } else {
+                    counts2[i] = counts2[month];
+                }
+            }
 
             // Chart 2
-            // Highcharts.chart('chart2', {
+            Highcharts.chart('chart2', {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: ''
+                },
+                xAxis: {
+                    categories: months,
+                },
+                yAxis: {
+                    allowDecimals: false,
+                    min: 0,
+                    title: {
+                        text: 'Counts'
+                    }
+                },
+                tooltip: {
+                    formatter: function() {
+                        return '<b>' + this.x + '</b><br/>' +
+                            this.series.name + ': ' + this.y + '<br/>' +
+                            'Total: ' + this.point.stackTotal;
+                    }
+                },
+                plotOptions: {
+                    column: {
+                        stacking: 'normal',
+                        pointWidth: 30
+                    }
+                },
+                series: [{
+                    name: '',
+                    data: counts1, //[5, 3, 4, 7, 2],
+                    stack: '',
+                    color: '#fa8c46'
+                }, {
+                    name: '',
+                    data: counts2, //[3, 4, 4, 2, 5],
+                    stack: '',
+                    color: '#216ff2'
 
-            //     chart: {
-            //         type: 'column'
-            //     },
-
-            //     title: {
-            //         text: ''
-            //     },
-
-            //     xAxis: {
-            //         categories: months,
-            //     },
-
-            //     yAxis: {
-            //         allowDecimals: false,
-            //         min: 0,
-            //         title: {
-            //             text: 'Counts'
-            //         }
-            //     },
-
-            //     tooltip: {
-            //         formatter: function() {
-            //             return '<b>' + this.x + '</b><br/>' +
-            //                 this.series.name + ': ' + this.y + '<br/>' +
-            //                 'Total: ' + this.point.stackTotal;
-            //         }
-            //     },
-
-            //     plotOptions: {
-            //         column: {
-            //             stacking: 'normal',
-            //             pointWidth: 30
-            //         }
-            //     },
-
-            //     series: [{
-            //         name: '',
-            //         data: counts1, //[5, 3, 4, 7, 2],
-            //         stack: '',
-            //         color: '#fa8c46'
-            //     }, {
-            //         name: '',
-            //         data: counts2, //[3, 4, 4, 2, 5],
-            //         stack: '',
-            //         color: '#216ff2'
-
-            //     }]
-            // });
+                }]
+            });
 
             $rootScope.loading = false;
 

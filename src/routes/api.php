26555072b6e871017da8c382fd78bcd5ccb74b14
Ramakrishnan <>
2020-03-16 13:59:55 +0530

    token enable for APIs

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,19 +1,19 @@
 <?php
 Route::group(['namespace' => 'Abs\RsaCasePkg\Api', 'middleware' => ['api']], function () {
 	Route::group(['prefix' => 'api/case-pkg'], function () {
-		Route::post('case/save', 'CaseController@save');
-
-		Route::post('activity/create', 'ActivityController@createActivity');
-
-		Route::post('get-invoiceable-activities', 'ActivityController@getInvoiceableActivities');
-
-		Route::post('activity/reject-po', 'ActivityController@rejectActivityPo');
-
-		Route::post('create-invoice', 'InvoiceController@createInvoice');
-
-		Route::post('get-invoice-list', 'InvoiceController@getList');
-		Route::post('get-invoice-details', 'InvoiceController@getDetails');
 		Route::group(['middleware' => ['auth:api']], function () {
+			Route::post('case/save', 'CaseController@save');
+
+			Route::post('activity/create', 'ActivityController@createActivity');
+
+			Route::post('get-invoiceable-activities', 'ActivityController@getInvoiceableActivities');
+
+			Route::post('activity/reject-po', 'ActivityController@rejectActivityPo');
+
+			Route::post('create-invoice', 'InvoiceController@createInvoice');
+
+			Route::post('get-invoice-list', 'InvoiceController@getList');
+			Route::post('get-invoice-details', 'InvoiceController@getDetails');
 		});
 	});
 });

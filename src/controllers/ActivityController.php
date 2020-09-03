35f0239b058bb2253698747b580673e4b1088fd1
Ram <ramrpk1234@gmail.com>
2020-09-03 16:45:17 +0530

    Update ActivityController.php

--- src/controllers/ActivityController.php
+++ src/controllers/ActivityController.php
@@ -2121,7 +2121,7 @@ class ActivityController extends Controller {
 			$excel->sheet('Activity Informations', function ($sheet) use ($activity_details_header, $activity_details_data) {
 				$sheet->fromArray($activity_details_data, NULL, 'A1');
 				$sheet->row(1, $activity_details_header);
-				$sheet->cells('A1:DD1', function ($cells) {
+				$sheet->cells('A1:DM1', function ($cells) {
 					$cells->setFont(array(
 						'size' => '10',
 						'bold' => true,

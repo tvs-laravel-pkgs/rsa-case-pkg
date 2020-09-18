bace3da7f8396fe7685ebb86fc1a290144ed7725
Ram <ramrpk1234@gmail.com>
2020-09-18 11:10:05 +0530

    Update InvoiceController.php

--- src/controllers/Api/InvoiceController.php
+++ src/controllers/Api/InvoiceController.php
@@ -16,6 +16,7 @@ class InvoiceController extends Controller {
 	private $successStatus = 200;
 
 	public function createInvoice(Request $request) {
+		dd($request->all());
 		ini_set('memory_limit', '-1');
 		ini_set('max_execution_time', 0);
 

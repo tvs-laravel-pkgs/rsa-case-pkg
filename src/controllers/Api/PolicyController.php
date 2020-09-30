d1fb879a346104f3b07338a834438e9b2d226ec7
karthick <karthick.r@uitoux.in>
2020-09-30 19:05:19 +0530

    policy entitlement updates completed.

--- src/controllers/Api/PolicyController.php
+++ src/controllers/Api/PolicyController.php
@@ -2,8 +2,11 @@
 
 namespace Abs\RsaCasePkg\Api;
 use App\Http\Controllers\Controller;
+use App\Membership;
 use DB;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Validator;
 
 class PolicyController extends Controller {
 	private $successStatus = 200;
@@ -30,10 +33,30 @@ class PolicyController extends Controller {
 	public function updatePolicyEntitlement(Request $request) {
 		ini_set('memory_limit', '-1');
 		ini_set('max_execution_time', 0);
-		dd($request->all());
 		DB::beginTransaction();
 		try {
-
+			$validator = Validator::make($request->all(), [
+				'membership_no' => [
+					'required:true',
+					Rule::exists('memberships', 'order_number'),
+				],
+				'expiry_reason' => [
+					'required:true',
+				],
+			]);
+			if ($validator->fails()) {
+				return response()->json([
+					'success' => false,
+					'error' => 'Validation Error',
+					'errors' => $validator->errors()->all(),
+				], $this->successStatus);
+			}
+			Membership::where('order_number', $request->membership_no)->update(['expiry_reason' => $request->expiry_reason]);
+			return response()->json([
+				'success' => true,
+				'message' => 'Updated successfully',
+			], $this->successStatus);
+			DB::commit();
 		} catch (\Exception $e) {
 			DB::rollBack();
 			return response()->json([

fe18b56d60d0bc2358ada4b624acbf9594eefaff
Ram <ramakrishnan@uitoux.in>
2020-11-17 16:35:32 +0530

    Update RsaCasePkgPermissionSeeder.php

--- src/database/seeds/RsaCasePkgPermissionSeeder.php
+++ src/database/seeds/RsaCasePkgPermissionSeeder.php
@@ -74,6 +74,12 @@ class RsaCasePkgPermissionSeeder extends Seeder {
 				'name' => 'backstep-activity',
 				'display_name' => 'Backstep Activity',
 			],
+			[
+				'display_order' => 9,
+				'parent' => 'activity-status',
+				'name' => 'select-case-submission-closing-date-activity',
+				'display_name' => 'Select Case Submission Closing Date',
+			],
 
 			//ACTIVITIES VERIFICATION
 			[

5e5400fe9453a1d57aec589dab142a392ecccbf3
Ram <ramakrishnan@uitoux.in>
2020-11-24 11:43:37 +0530

    Update ActivityController.php

--- src/controllers/ActivityController.php
+++ src/controllers/ActivityController.php
@@ -2225,6 +2225,12 @@ class ActivityController extends Controller {
 			} else {
 				$submission_closing_date = date('d-m-Y H:i:s', strtotime("+3 months", strtotime($activity->case->created_at)));
 			}
+			if (!empty($activity->invoice) && !empty($activity->invoice->created_at)) {
+				$inv_created_at = date('d-m-Y', strtotime(str_replace('/', '-', $activity->invoice->created_at)));
+			} else {
+				$inv_created_at = '';
+			}
+
 			$activity_details_data[] = [
 				$activity->id,
 				$activity->case->number,
@@ -2273,7 +2279,7 @@ class ActivityController extends Controller {
 				$activity->exceptional_reason != NULL ? $activity->exceptional_reason : '',
 				// $activity->invoice ? ($activity->asp->has_gst == 1 && $activity->asp->is_auto_invoice == 0 ? ($activity->invoice->invoice_no) : ($activity->invoice->invoice_no . '-' . $activity->invoice->id)) : '',
 				$activity->invoice ? $activity->invoice->invoice_no : '',
-				$activity->invoice ? (!empty($activity->invoice->created_at) ? date('d-m-Y', strtotime($activity->invoice->created_at)) : '') : '',
+				$inv_created_at,
 				$activity->invoice ? preg_replace("/(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/i", "$1,", str_replace(",", "", number_format($activity->invoice->invoice_amount, 2))) : '',
 				$activity->invoice ? ($activity->invoice->invoiceStatus ? $activity->invoice->invoiceStatus->name : '') : '',
 				// '',

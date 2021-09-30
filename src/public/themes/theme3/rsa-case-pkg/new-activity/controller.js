b8820f17fc59a81527d903c660fb9be88263589b
Gowtham-uitoux <83208219+Gowtham-uitoux@users.noreply.github.com>
2021-09-30 09:56:11 +0530

    Break down location implementation in ASP data entry form

--- src/public/themes/theme3/rsa-case-pkg/new-activity/controller.js
+++ src/public/themes/theme3/rsa-case-pkg/new-activity/controller.js
@@ -97,6 +97,7 @@ app.component('newActivityUpdateDetails', {
             //self.data.unpaid_amount = response.data.activity.unpaid_amount;
             self.service_type_id = response.data.activity.service_type_id;
             self.range_limit = response.data.range_limit;
+            self.bd_location = response.data.case_details.bd_location;
             $rootScope.loading = false;
             if (self.for_deffer_activity) {
                 $('.resolve_comment').show();

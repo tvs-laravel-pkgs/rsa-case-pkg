dae35237a24db7eb9baea05c7811b1320f809d4d
Ram <ramakrishnan@uitoux.in>
2021-10-05 14:54:29 +0530

    Update Activity.php

--- src/models/Activity.php
+++ src/models/Activity.php
@@ -64,6 +64,8 @@ class Activity extends Model {
 		'general_remarks',
 	];
 
+	// Relationships --------------------------------------------------------------
+
 	public function financeStatus() {
 		return $this->belongsTo('Abs\RsaCasePkg\ActivityFinanceStatus', 'finance_status_id');
 	}
@@ -150,6 +152,27 @@ class Activity extends Model {
 
 	public function dataSource() {
 		return $this->belongsTo('App\Config', 'data_src_id');
+	}
+
+	// Static Funcs --------------------------------------------------------------
+
+	public static function searchMembershipTicket($r) {
+		$key = $r->key;
+		$list = self::select([
+			'activities.id',
+			'cases.number',
+			'asps.asp_code',
+			'service_types.name as service_type',
+		])
+			->join('cases', 'cases.id', 'activities.case_id')
+			->join('asps', 'asps.id', 'activities.asp_id')
+			->join('service_types', 'service_types.id', 'activities.service_type_id')
+			->where(function ($q) use ($key) {
+				$q->where('cases.number', 'like', '%' . $key . '%')
+				;
+			})
+			->get();
+		return response()->json($list);
 	}
 
 	public static function getFormData($id = NULL, $for_deffer_activity) {

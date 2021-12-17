335cc3681dba32501e7db8d671d8fe814902da9e
Ram <ramakrishnan@uitouxsolutions.com>
2021-12-17 13:59:11 +0530

    Update ActivityController.php

--- src/controllers/ActivityController.php
+++ src/controllers/ActivityController.php
@@ -3402,7 +3402,7 @@ class ActivityController extends Controller {
 					))->setBackground('#CCC9C9');
 				});
 			});
-		})->export('xls');
+		})->export('xlsx');
 
 		return redirect()->back()->with(['success' => 'exported!']);
 	}

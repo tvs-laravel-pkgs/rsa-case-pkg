7cbbcb5e3d8778ff3ddaaf4f4006f59d87b06293
Ram <45380286+ram-uitoux@users.noreply.github.com>
2022-03-14 20:38:38 +0530

    Update ActivityController.php

--- src/controllers/ActivityController.php
+++ src/controllers/ActivityController.php
@@ -4259,7 +4259,7 @@ class ActivityController extends Controller {
 				$sheet->setAutoSize(false);
 				$sheet->fromArray($activity_details_data, NULL, 'A1');
 				$sheet->row(1, $activity_details_header);
-				$sheet->cells('A1:EO1', function ($cells) {
+				$sheet->cells('A1:EZ1', function ($cells) {
 					$cells->setFont(array(
 						'size' => '10',
 						'bold' => true,

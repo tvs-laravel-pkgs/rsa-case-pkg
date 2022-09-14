ced65d21aa2ef27fd1095baf7a6a1e0860914c74
Ram <45380286+ram-uitoux@users.noreply.github.com>
2022-09-14 13:35:52 +0530

    Update Activity.php

--- src/models/Activity.php
+++ src/models/Activity.php
@@ -1144,7 +1144,11 @@ class Activity extends Model {
 											$activity->status_id = 6;
 										}
 									} else {
-										$activity->status_id = 2; //ASP Rejected CC Details - Waiting for ASP Data Entry
+										if ($activity->is_asp_data_entry_done == 1) {
+											$activity->status_id = 6; //ASP Completed Data Entry - Waiting for L1 Individual Verification
+										} else {
+											$activity->status_id = 2; //ASP Rejected CC Details - Waiting for ASP Data Entry
+										}
 									}
 								} else {
 									//ON HOLD
@@ -1197,7 +1201,11 @@ class Activity extends Model {
 									if ($case->status_id == 4) {
 										//IF ROS ASP then changes status as Waitin for ASP data entry. If not change status as on hold
 										if ($asp->is_ros_asp == 1) {
-											$activity->status_id = 2; //ASP Rejected CC Details - Waiting for ASP Data Entry
+											if ($activity->is_asp_data_entry_done == 1) {
+												$activity->status_id = 6; //ASP Completed Data Entry - Waiting for L1 Individual Verification
+											} else {
+												$activity->status_id = 2; //ASP Rejected CC Details - Waiting for ASP Data Entry
+											}
 										} else {
 											//ON HOLD
 											$activity->status_id = 17;
@@ -1268,7 +1276,7 @@ class Activity extends Model {
 										$activityLog->save();
 
 										//SEND BREAKDOWN OR EMPTY RETURN CHARGES WHATSAPP SMS TO ASP
-										if ($invoiceAmountCalculatedActivity->asp && !empty($invoiceAmountCalculatedActivity->asp->whatsapp_number) && $activity->financeStatus && $activity->financeStatus->po_eligibility_type_id != 342) {
+										if ($invoiceAmountCalculatedActivity->asp && !empty($invoiceAmountCalculatedActivity->asp->whatsapp_number)) {
 											$invoiceAmountCalculatedActivity->sendBreakdownOrEmptyreturnChargesWhatsappSms();
 										}
 
@@ -1303,7 +1311,12 @@ class Activity extends Model {
 												$statusId = 6;
 											}
 										} else {
-											$statusId = 2; //ASP Rejected CC Details - Waiting for ASP Data Entry
+											if ($caseActivity->is_asp_data_entry_done == 1) {
+												//ASP Completed Data Entry - Waiting for L1 Individual Verification
+												$statusId = 6;
+											} else {
+												$statusId = 2; //ASP Rejected CC Details - Waiting for ASP Data Entry
+											}
 										}
 										$caseActivity->update([
 											'status_id' => $statusId,

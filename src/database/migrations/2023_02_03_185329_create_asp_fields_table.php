429b91956ea6abd149ff67b31602f70b6931dc46
Keerthana-Uitoux <keerthana@uitoux.in>
2023-02-03 19:33:02 +0530

    migrations added for asp amendments

---
+++ src/database/migrations/2023_02_03_185329_create_asp_fields_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAspFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asp_fields', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('field_name' , 255);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asp_fields');
+    }
+}
